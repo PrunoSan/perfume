0d6e8426102070d80f968efb72aee2691c348596
Bruno Santos <bruno-santosm@hotmail.com>
2022-11-07 15:16:52 -0300

    V5 Perfume
    Bibliotecas Js:
    -AOS
    -Owl Carousel

--- js/main.js
+++ js/main.js
@@ -37,14 +37,8 @@ window.addEventListener('scroll', function () {
   }
 });
 
-var myVar;
 
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
-}
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
+
+
 
